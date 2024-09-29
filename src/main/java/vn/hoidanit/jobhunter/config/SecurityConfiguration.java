ec9931e4671d9cb185136ae5102181caf02b8bd1
nguyen <nguyenthcs430@gmail.com>
2024-09-29 10:18:21 +0700

    final permission

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package vn.hoidanit.jobhunter.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,15 +45,26 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http,
             CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
+        String[] whiteList = {
+                "/",
+                "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
+                "/storage/**",
+                // "/api/v1/email/**",
+                // "/v3/api-docs/**",
+                // "/swagger-ui/**",
+                // "/swagger-ui.html"
+        };
+
         http
                 .csrf(c -> c.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         authz -> authz
-                                .requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh",
-                                        "/api/v1/auth/register",
-                                        "/storage/**", "/api/v1/companies/**", "/api/v1/jobs/**")
-                                .permitAll()
+                                .requestMatchers(whiteList).permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/companies/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/jobs/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/skills/**").permitAll()
+
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
--- src/main/java/vn/hoidanit/jobhunter/util/error/GlobalException.java
+++ src/main/java/vn/hoidanit/jobhunter/util/error/GlobalException.java
@@ -33,9 +33,9 @@ public class GlobalException {
     }
 
     @ExceptionHandler(value = {
-        NoResourceFoundException.class,
+            NoResourceFoundException.class,
     })
-    public ResponseEntity<RestResponse<Object>> handleNotFoundException(Exception ex){
+    public ResponseEntity<RestResponse<Object>> handleNotFoundException(Exception ex) {
         RestResponse<Object> res = new RestResponse<Object>();
         res.setStatusCode(HttpStatus.NOT_FOUND.value());
         res.setError(ex.getMessage());
@@ -59,15 +59,26 @@ public class GlobalException {
     }
 
     @ExceptionHandler(value = {
-        StorageException.class,
-       
-})
-public ResponseEntity<RestResponse<Object>> handleFileUploadException(Exception ex) {
-    RestResponse<Object> res = new RestResponse<Object>();
-    res.setStatusCode(HttpStatus.BAD_REQUEST.value());
-    res.setError(ex.getMessage());
-    res.setMessage("Exception upload file....");
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
-}
+            StorageException.class,
+
+    })
+    public ResponseEntity<RestResponse<Object>> handleFileUploadException(Exception ex) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(ex.getMessage());
+        res.setMessage("Exception upload file....");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+
+    @ExceptionHandler(value = {
+            PermissionException.class,
+    })
+    public ResponseEntity<RestResponse<Object>> handlePermissionException(Exception ex) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.FORBIDDEN.value());
+        res.setError("Forbidden");
+        res.setMessage(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+    }
 
 }
