c6316d0d5d3e78e5321f6e9d692c123e7270ec3a
nguyen <nguyenthcs430@gmail.com>
2024-10-04 07:50:21 +0700

    final send email

--- src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
@@ -1,33 +1,33 @@
 package vn.hoidanit.jobhunter.service;
 
-// import java.nio.charset.StandardCharsets;
-// import org.springframework.mail.MailException;
+import java.nio.charset.StandardCharsets;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-// import org.springframework.mail.javamail.MimeMessageHelper;
-// import org.springframework.scheduling.annotation.Async;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-// import org.thymeleaf.context.Context;
-// import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
-// import jakarta.mail.MessagingException;
-// import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
 
     private final MailSender mailSender;
     private final JavaMailSender javaMailSender;
-    // private final SpringTemplateEngine templateEngine;
+    private final SpringTemplateEngine templateEngine;
 
     public EmailService(MailSender mailSender,
-            JavaMailSender javaMailSender
-            // SpringTemplateEngine templateEngine
+            JavaMailSender javaMailSender,
+            SpringTemplateEngine templateEngine
             ) {
         this.mailSender = mailSender;
         this.javaMailSender = javaMailSender;
-        // this.templateEngine = templateEngine;
+        this.templateEngine = templateEngine;
     }
 
     public void sendSimpleEmail() {
@@ -38,34 +38,34 @@ public class EmailService {
         this.mailSender.send(msg);
     }
 
-    // public void sendEmailSync(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
-    //     // Prepare message using a Spring helper
-    //     MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
-    //     try {
-    //         MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, StandardCharsets.UTF_8.name());
-    //         message.setTo(to);
-    //         message.setSubject(subject);
-    //         message.setText(content, isHtml);
-    //         this.javaMailSender.send(mimeMessage);
-    //     } catch (MailException | MessagingException e) {
-    //         System.out.println("ERROR SEND EMAIL: " + e);
-    //     }
-    // }
+    public void sendEmailSync(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
+        // Prepare message using a Spring helper
+        MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, StandardCharsets.UTF_8.name());
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(content, isHtml);
+            this.javaMailSender.send(mimeMessage);
+        } catch (MailException | MessagingException e) {
+            System.out.println("ERROR SEND EMAIL: " + e);
+        }
+    }
 
-    // @Async
-    // public void sendEmailFromTemplateSync(
-    //         String to,
-    //         String subject,
-    //         String templateName,
-    //         String username,
-    //         Object value) {
+    @Async
+    public void sendEmailFromTemplateSync(
+            String to,
+            String subject,
+            String templateName,
+            String username,
+            Object value) {
 
-    //     Context context = new Context();
-    //     context.setVariable("name", username);
-    //     context.setVariable("jobs", value);
+        Context context = new Context();
+        context.setVariable("name", username);
+        context.setVariable("jobs", value);
 
-    //     String content = templateEngine.process(templateName, context);
-    //     this.sendEmailSync(to, subject, content, false, true);
-    // }
+        String content = templateEngine.process(templateName, context);
+        this.sendEmailSync(to, subject, content, false, true);
+    }
 
 }
